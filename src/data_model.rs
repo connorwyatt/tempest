86b7f4b59ca86879194c3700661b08e2e7460ccf
Connor Wyatt <connorwyatt1@gmail.com>
2023-11-13 23:30:13 +0000

    Refactor parsing code

--- src/data_model.rs
+++ src/data_model.rs
@@ -14,67 +14,80 @@ impl DataModel {
         let mut nodes = Vec::new();
         let mut links = Vec::new();
 
-        let node_check_regex = regex!("^(?<node_type>command|aggregate|event|policy)"i);
-        let node_regex = regex!(
-            r#"^(command|aggregate|event|policy)\s*\((?<id>[a-z0-9]*)\)?\s*:\s*"(?<text>.*)"$"#i
-        );
-        let link_check_regex = regex!("^link"i);
-        let link_regex =
-            regex!(r#"^link\s*:\s*(?<start_id>[a-z0-9]*)\s*->\s*(?<end_id>[a-z0-9]*)$"#i);
-
-        for (index, line) in input.lines().enumerate() {
+        for (index, line) in input.lines().map(|x| x.trim()).enumerate() {
             let line_number = index + 1;
-            let trimmed_line = line.trim();
 
-            if trimmed_line.is_empty() {
+            if line.is_empty() {
                 continue;
             }
 
-            if let Some(check_captures) = node_check_regex.captures(trimmed_line) {
-                let node_type_string = check_captures
-                    .name("node_type")
-                    .unwrap()
-                    .as_str()
-                    .to_string();
-
-                let node_type = node_type_string.parse().unwrap();
-
-                let Some(captures) = node_regex.captures(trimmed_line) else {
-                    eprintln!(
-                        "Could not parse {} (line {}):",
-                        node_type_string.to_lowercase(),
-                        line_number
-                    );
-                    eprintln!("    \"{}\"", trimmed_line);
-                    exit(1);
-                };
-
-                let node = Node {
-                    id: captures.name("id").unwrap().as_str().to_string(),
-                    node_type,
-                    text: captures.name("text").unwrap().as_str().to_string(),
-                };
+            if let Some(node) = Self::parse_node(line, line_number) {
                 nodes.push(node);
-            } else if link_check_regex.is_match(trimmed_line) {
-                let Some(captures) = link_regex.captures(trimmed_line) else {
-                    eprintln!("Could not parse link (line {}):", line_number);
-                    eprintln!("    \"{}\"", trimmed_line);
-                    exit(1);
-                };
-
-                let link = Link {
-                    start_id: captures.name("start_id").unwrap().as_str().to_string(),
-                    end_id: captures.name("end_id").unwrap().as_str().to_string(),
-                };
+            } else if let Some(link) = Self::parse_link(line, line_number) {
                 links.push(link);
             } else {
                 eprintln!("Could not parse (line {}):", line_number);
-                eprintln!("    \"{}\"", trimmed_line);
+                eprintln!("    \"{}\"", line);
                 exit(1);
             }
         }
 
         Self { nodes, links }
+    }
+
+    fn parse_node(input: &str, line_number: usize) -> Option<Node> {
+        let check_regex = regex!("^(?<node_type>command|aggregate|event|policy)"i);
+        let regex = regex!(
+            r#"^(command|aggregate|event|policy)\s*\((?<id>[a-z0-9]*)\)?\s*:\s*"(?<text>.*)"$"#i
+        );
+
+        let Some(check_captures) = check_regex.captures(input) else {
+            return None;
+        };
+
+        let node_type_string = check_captures
+            .name("node_type")
+            .unwrap()
+            .as_str()
+            .to_string();
+
+        let node_type = node_type_string.parse().unwrap();
+
+        let Some(captures) = regex.captures(input) else {
+            eprintln!(
+                "Could not parse {} (line {}):",
+                node_type_string.to_lowercase(),
+                line_number
+            );
+            eprintln!("    \"{}\"", input);
+            exit(1);
+        };
+
+        Some(Node {
+            id: captures.name("id").unwrap().as_str().to_string(),
+            node_type,
+            text: captures.name("text").unwrap().as_str().to_string(),
+        })
+    }
+
+    fn parse_link(input: &str, line_number: usize) -> Option<Link> {
+        let check_regex = regex!("^link"i);
+        let regex = regex!(r#"^link\s*:\s*(?<start_id>[a-z0-9]*)\s*->\s*(?<end_id>[a-z0-9]*)$"#i);
+
+        if !check_regex.is_match(input) {
+            return None;
+        }
+
+        let Some(captures) = regex.captures(input) else {
+            eprintln!("Could not parse link (line {}):", line_number);
+            eprintln!("    \"{}\"", input);
+            exit(1);
+        };
+
+        Some(Link {
+            start_id: captures.name("start_id").unwrap().as_str().to_string(),
+            end_id: captures.name("end_id").unwrap().as_str().to_string(),
+        })
     }
 }
 
